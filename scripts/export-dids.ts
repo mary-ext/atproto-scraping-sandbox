b86f0cf5e4dc96a9f8d5835fe3b4c82a3d4a2a23
Mary <git@mary.my.id>
2024-05-05 10:11:16 +0700

    initial commit

---
+++ scripts/export-dids.ts
@@ -0,0 +1,328 @@
+import { BskyXRPC } from '@mary/bluesky-client';
+
+import { differenceInDays } from 'date-fns/differenceInDays';
+import * as tld from 'tldts';
+import * as v from '@badrap/valita';
+
+import {
+	serializedState,
+	type DidWebInfo,
+	type InstanceInfo,
+	type LabelerInfo,
+	type SerializedState,
+} from '../src/state';
+
+import { didDocument, type DidDocument } from '../src/utils/did';
+import { PromiseQueue } from '../src/utils/pqueue';
+import { LineBreakStream, TextDecoderStream } from '../src/utils/stream';
+import { compareString } from '../src/utils/misc';
+
+const now = Date.now();
+
+const env = v.object({ STATE_FILE: v.string() }).parse(process.env, { mode: 'passthrough' });
+
+let state: SerializedState | undefined;
+
+// Read existing state file
+{
+	let json: unknown;
+
+	try {
+		json = await Bun.file(env.STATE_FILE).json();
+	} catch {}
+
+	if (json !== undefined) {
+		state = serializedState.parse(json);
+	}
+}
+
+// Global states
+const didWebs = new Map<string, DidWebInfo>(state ? Object.entries(state.firehose.didWebs) : []);
+const pdses = new Map<string, InstanceInfo>(state ? Object.entries(state.pdses) : []);
+const labelers = new Map<string, LabelerInfo>(state ? Object.entries(state.labelers) : []);
+
+const queue = new PromiseQueue();
+
+let plcCursor: string | undefined = state?.plc.cursor;
+let firehoseCursor: string | undefined = state?.firehose.cursor;
+
+// Iterate through PLC events
+{
+	const limit = 1000;
+	let after: string | undefined = plcCursor;
+
+	console.log(`crawling plc.bsky-sandbox.dev`);
+	console.log(`  starting ${plcCursor || '<root>'}`);
+
+	do {
+		const url = `https://plc.bsky-sandbox.dev/export` + `?count=${limit}` + (after ? `&after=${after}` : '');
+
+		const response = await get(url);
+		const stream = response.body!.pipeThrough(new TextDecoderStream()).pipeThrough(new LineBreakStream());
+
+		after = undefined;
+
+		let count = 0;
+
+		for await (const raw of stream) {
+			const json = JSON.parse(raw) as ExportEntry;
+			const { did, operation, createdAt } = json;
+
+			if (operation.type === 'plc_operation') {
+				const pds = getEndpoint(operation.services.atproto_pds?.endpoint);
+				const labeler = getEndpoint(operation.services.atproto_labeler?.endpoint);
+
+				if (pds) {
+					if (!pdses.has(pds)) {
+						console.log(`  found pds: ${pds}`);
+						pdses.set(pds, {});
+					}
+				}
+
+				if (labeler) {
+					if (!labelers.has(labeler)) {
+						console.log(`  found labeler: ${labeler}`);
+						labelers.set(labeler, { did });
+					} else {
+						labelers.get(labeler)!.did = did;
+					}
+				}
+			}
+
+			count++;
+			after = createdAt;
+		}
+
+		if (after) {
+			plcCursor = after;
+		}
+
+		if (count < limit) {
+			break;
+		}
+	} while (after !== undefined);
+
+	console.log(`  ending ${plcCursor || '<root>'}`);
+
+	interface ExportEntry {
+		did: string;
+		operation: PlcOperation;
+		cid: string;
+		nullified: boolean;
+		createdAt: string;
+	}
+
+	type PlcOperation = LegacyGenesisOp | OperationOp | TombstoneOp;
+
+	interface OperationOp {
+		type: 'plc_operation';
+		/** did:key[] */
+		rotationKeys: string[];
+		/** Record<string, did:key> */
+		verificationMethods: Record<string, string>;
+		alsoKnownAs: string[];
+		services: Record<string, Service>;
+		prev: string | null;
+		sig: string;
+	}
+
+	interface TombstoneOp {
+		type: 'plc_tombstone';
+		prev: string;
+		sig: string;
+	}
+
+	interface LegacyGenesisOp {
+		type: 'create';
+		/** did:key */
+		signingKey: string;
+		/** did:key */
+		recoveryKey: string;
+		handle: string;
+		service: string;
+		prev: string | null;
+		sig: string;
+	}
+
+	interface Service {
+		type: string;
+		endpoint: string;
+	}
+}
+
+// Iterate through firehose' known repositories
+{
+	const rpc = new BskyXRPC({ service: 'https://bgs.bsky-sandbox.dev' });
+
+	let cursor: string | undefined = firehoseCursor;
+
+	console.log(`crawling bgs.bsky-sandbox.dev`);
+	console.log(`  starting ${cursor || '<root>'}`);
+
+	do {
+		const { data } = await rpc.get('com.atproto.sync.listRepos', {
+			params: {
+				cursor: cursor,
+				limit: 1_000,
+			},
+		});
+
+		for (const { did } of data.repos) {
+			if (did.startsWith('did:web:')) {
+				if (!didWebs.has(did)) {
+					console.log(`  found ${did}`);
+					didWebs.set(did, {});
+				}
+			}
+		}
+
+		cursor = data.cursor;
+
+		if (cursor) {
+			firehoseCursor = cursor;
+		}
+	} while (cursor !== undefined);
+
+	console.log(`  ending ${firehoseCursor || '<root>'}`);
+}
+
+// Retrieve PDS information from known did:web identities
+{
+	console.log(`crawling known did:web identities`);
+	const dids = Array.from(didWebs.keys());
+
+	await Promise.all(
+		dids.map((did) => {
+			return queue.add(async () => {
+				const host = did.slice(8);
+				const obj = didWebs.get(did)!;
+
+				try {
+					const signal = AbortSignal.timeout(15_000);
+					const res = await get(`https://${host}/.well-known/did.json`, signal);
+
+					const json = (await res.json()) as unknown;
+					const doc = didDocument.parse(json, { mode: 'passthrough' });
+
+					const pds = getPdsEndpoint(doc);
+					const labeler = getLabelerEndpoint(doc);
+
+					console.log(`  ${did}: pass`);
+
+					if (pds && obj.pds !== pds) {
+						if (!pdses.has(pds)) {
+							console.log(`    found pds: ${pds}`);
+							pdses.set(pds, {});
+						}
+					}
+
+					if (labeler && obj.labeler !== labeler) {
+						if (!labelers.has(labeler)) {
+							console.log(`    found labeler: ${labeler}`);
+							labelers.set(labeler, { did });
+						} else {
+							labelers.get(labeler)!.did = did;
+						}
+					}
+
+					obj.errorAt = undefined;
+					obj.pds = pds;
+					obj.labeler = labeler;
+				} catch (err) {
+					const errorAt = obj.errorAt;
+
+					if (errorAt === undefined) {
+						obj.errorAt = now;
+					} else if (differenceInDays(now, errorAt) > 7) {
+						didWebs.delete(did);
+					}
+
+					console.log(`  ${did}: fail`);
+				}
+			});
+		}),
+	);
+}
+
+// Persist the state
+{
+	const serialized: SerializedState = {
+		firehose: {
+			cursor: firehoseCursor,
+			didWebs: Object.fromEntries(Array.from(didWebs).sort(([aDid], [bDid]) => compareString(aDid, bDid))),
+		},
+		plc: {
+			cursor: plcCursor,
+		},
+
+		pdses: Object.fromEntries(Array.from(pdses).sort(([aHref], [bHref]) => compareString(aHref, bHref))),
+		labelers: Object.fromEntries(
+			Array.from(labelers).sort(([aHref], [bHref]) => compareString(aHref, bHref)),
+		),
+	};
+
+	await Bun.write(env.STATE_FILE, JSON.stringify(serialized, null, '\t'));
+}
+
+async function get(url: string, signal?: AbortSignal): Promise<Response> {
+	const response = await fetch(url, { signal });
+
+	if (response.status === 429) {
+		const delay = 90_000;
+
+		console.log(`[-] ratelimited, waiting ${delay} ms`);
+
+		await sleep(delay);
+		return get(url, signal);
+	}
+
+	if (!response.ok) {
+		throw new Error(`got ${response.status} from ${url}`);
+	}
+
+	return response;
+}
+
+function sleep(ms: number): Promise<void> {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function getPdsEndpoint(doc: DidDocument): string | undefined {
+	return getServiceEndpoint(doc, '#atproto_pds', 'AtprotoPersonalDataServer');
+}
+
+function getLabelerEndpoint(doc: DidDocument): string | undefined {
+	return getServiceEndpoint(doc, '#atproto_labeler', 'AtprotoLabeler');
+}
+
+function getServiceEndpoint(doc: DidDocument, serviceId: string, serviceType: string): string | undefined {
+	const did = doc.id;
+
+	const didServiceId = did + serviceId;
+	const found = doc.service?.find((service) => service.id === serviceId || service.id === didServiceId);
+
+	if (!found || found.type !== serviceType || typeof found.serviceEndpoint !== 'string') {
+		return undefined;
+	}
+
+	return getEndpoint(found.serviceEndpoint);
+}
+function getEndpoint(urlStr: string | undefined): string | undefined {
+	if (urlStr === undefined) {
+		return undefined;
+	}
+
+	// @ts-expect-error
+	const url = URL.parse(urlStr) as URL | null;
+
+	if (!url || !(url.protocol === 'http:' || url.protocol === 'https:')) {
+		return undefined;
+	}
+
+	const parsed = tld.parse(url.hostname);
+	if (!parsed.domain || !(parsed.isIcann || parsed.isIp)) {
+		return undefined;
+	}
+
+	return url.href;
+}
---
+++ scripts/generate-markdown.ts
@@ -0,0 +1,265 @@
+import { BskyXRPC } from '@mary/bluesky-client';
+import { XRPCError } from '@mary/bluesky-client/xrpc';
+
+import { differenceInDays } from 'date-fns/differenceInDays';
+import * as v from '@badrap/valita';
+
+import { serializedState, type InstanceInfo, type LabelerInfo, type SerializedState } from '../src/state';
+
+import { PromiseQueue } from '../src/utils/pqueue';
+
+const now = Date.now();
+
+const env = v
+	.object({ STATE_FILE: v.string(), RESULT_FILE: v.string() })
+	.parse(process.env, { mode: 'passthrough' });
+
+let state: SerializedState | undefined;
+
+// Read existing state file
+{
+	let json: unknown;
+
+	try {
+		json = await Bun.file(env.STATE_FILE).json();
+	} catch {}
+
+	if (json !== undefined) {
+		state = serializedState.parse(json);
+	}
+}
+
+// Some schema validations
+const pdsDescribeServerResponse = v.object({
+	availableUserDomains: v.array(v.string()),
+	did: v.string(),
+
+	contact: v.object({ email: v.string().optional() }).optional(),
+	inviteCodeRequired: v.boolean().optional(),
+	links: v.object({ privacyPolicy: v.string().optional(), termsOfService: v.string().optional() }).optional(),
+	phoneVerificationRequired: v.boolean().optional(),
+});
+
+const labelerQueryLabelsResponse = v.object({
+	cursor: v.string().optional(),
+	labels: v.array(
+		v.object({
+			src: v.string(),
+			uri: v.string(),
+			val: v.string(),
+			cts: v.string(),
+
+			cid: v.string().optional(),
+			exp: v.string().optional(),
+			neg: v.boolean().optional(),
+			sig: v.object({ $bytes: v.string() }).optional(),
+			ver: v.number().optional(),
+		}),
+	),
+});
+
+const offHealthResponse = v.object({
+	version: v.string(),
+});
+
+// Global states
+const pdses = new Map<string, InstanceInfo>(state ? Object.entries(state.pdses) : []);
+const labelers = new Map<string, LabelerInfo>(state ? Object.entries(state.labelers) : []);
+
+const queue = new PromiseQueue();
+
+// Connect to PDSes
+console.log(`crawling known pdses`);
+
+const pdsResults = await Promise.all(
+	Array.from(pdses, ([href, obj]) => {
+		return queue.add(async () => {
+			const host = new URL(href).host;
+			const rpc = new BskyXRPC({ service: href });
+
+			const signal = AbortSignal.timeout(15_000);
+			const meta = await rpc
+				.get('com.atproto.server.describeServer', { signal })
+				.then(({ data: rawData }) => {
+					const data = pdsDescribeServerResponse.parse(rawData, { mode: 'passthrough' });
+
+					if (data.did !== `did:web:${host}`) {
+						throw new Error(`did mismatch`);
+					}
+
+					return data;
+				})
+				.catch(() => null);
+
+			if (meta === null) {
+				const errorAt = obj.errorAt;
+
+				if (errorAt === undefined) {
+					obj.errorAt = now;
+				} else if (differenceInDays(now, errorAt) > 7) {
+					pdses.delete(href);
+				}
+
+				console.log(`  ${host}: fail`);
+				return;
+			}
+
+			const version = await getVersion(rpc, obj.version);
+
+			obj.version = version;
+			obj.errorAt = undefined;
+
+			console.log(`  ${host}: pass`);
+			return { host, meta, version };
+		});
+	}),
+).then((results) => results.filter((r) => r !== undefined));
+
+// Connect to labelers
+console.log(`crawling known labelers`);
+
+const labelerResults = await Promise.all(
+	Array.from(labelers, async ([href, obj]) => {
+		return queue.add(async () => {
+			const host = new URL(href).host;
+			const rpc = new BskyXRPC({ service: href });
+
+			const signal = AbortSignal.timeout(15_000);
+			const meta = await rpc
+				.get('com.atproto.label.queryLabels', { signal: signal, params: { uriPatterns: ['*'], limit: 1 } })
+				.then(({ data: rawData }) => labelerQueryLabelsResponse.parse(rawData, { mode: 'passthrough' }))
+				.catch(() => null);
+
+			if (meta === null) {
+				const errorAt = obj.errorAt;
+
+				if (errorAt === undefined) {
+					obj.errorAt = now;
+				} else if (differenceInDays(now, errorAt) > 7) {
+					labelers.delete(href);
+				}
+
+				console.log(`  ${host}: fail`);
+				return;
+			}
+
+			const version = await getVersion(rpc, obj.version);
+
+			obj.version = version;
+			obj.errorAt = undefined;
+
+			console.log(`  ${host}: pass`);
+			return { host, version };
+		});
+	}),
+).then((results) => results.filter((r) => r !== undefined));
+
+// Markdown stuff
+{
+	const PDS_RE = /(?<=<!-- pds-start -->)[^]*(?=<!-- pds-end -->)/;
+	const LABELER_RE = /(?<=<!-- labeler-start -->)[^]*(?=<!-- labeler-end -->)/;
+
+	const template = `# Scraped AT Protocol sandbox instances
+
+Last updated: {{time}}[^1]
+
+Found by enumerating plc.bsky-sandbox.dev and bgs.bsky-sandbox.dev.
+
+## Personal data servers
+
+<!-- pds-start --><!-- pds-end -->
+
+## Labelers
+
+<!-- labeler-start --><!-- labeler-end -->
+
+[^1]: Reflecting actual changes, not when the scraper was last run
+`;
+
+	let pdsTable = `
+| PDS | Open? | Version |
+| --- | --- | --- |
+`;
+
+	let labelerTable = `
+| Labeler | Version |
+| --- | --- |
+`;
+
+	// Generate the PDS table
+	for (const { host, meta, version } of pdsResults) {
+		const v = version || (version === null ? 'N/A' : '???');
+
+		pdsTable += `| ${host} | ${!meta.inviteCodeRequired ? 'Yes' : 'No'} | ${v} |\n`;
+	}
+
+	// Generate the labeler table
+	for (const { host, version } of labelerResults) {
+		const v = version || (version === null ? 'N/A' : '???');
+
+		labelerTable += `| ${host} | ${v} |\n`;
+	}
+
+	// Read existing Markdown file, check if it's equivalent
+	let shouldWrite = true;
+
+	try {
+		const source = await Bun.file(env.RESULT_FILE).text();
+
+		if (PDS_RE.exec(source)?.[0] === pdsTable && LABELER_RE.exec(source)?.[0] === labelerTable) {
+			shouldWrite = false;
+		}
+	} catch {}
+
+	// Write the markdown file
+	if (shouldWrite) {
+		const final = template
+			.replace('{{time}}', new Date().toISOString())
+			.replace(PDS_RE, pdsTable)
+			.replace(LABELER_RE, labelerTable);
+
+		await Bun.write(env.RESULT_FILE, final);
+		console.log(`wrote to ${env.RESULT_FILE}`);
+	} else {
+		console.log(`writing skipped`);
+	}
+}
+
+// Persist the state
+{
+	const serialized: SerializedState = {
+		firehose: {
+			cursor: state?.firehose.cursor,
+			didWebs: state?.firehose.didWebs || {},
+		},
+		plc: {
+			cursor: state?.plc.cursor,
+		},
+
+		pdses: Object.fromEntries(Array.from(pdses)),
+		labelers: Object.fromEntries(Array.from(labelers)),
+	};
+
+	await Bun.write(env.STATE_FILE, JSON.stringify(serialized, null, '\t'));
+}
+
+async function getVersion(rpc: BskyXRPC, prev: string | null | undefined) {
+	// skip if the response previously returned null (not official distrib)
+	if (prev === null) {
+		return null;
+	}
+
+	try {
+		// @ts-expect-error: undocumented endpoint
+		const { data: rawData } = await rpc.get('_health', {});
+		const { version } = offHealthResponse.parse(rawData, { mode: 'passthrough' });
+
+		return /^[0-9a-f]{40}$/.test(version) ? `git-${version.slice(0, 7)}` : version;
+	} catch (err) {
+		if (err instanceof XRPCError && err.status !== 404) {
+			return undefined;
+		}
+	}
+
+	return null;
+}
